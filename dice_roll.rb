47756745b42a5c96a07ef467b8d5fd11c5e45a2e
Oleg Chursin <olegchursin@gmail.com>
2017-11-25 18:42:42 +0000

    Done.

--- dice_roll.rb
+++ dice_roll.rb
@@ -2,6 +2,12 @@
 
 # Feel free to google "how to generate a random number in ruby"
 
+#Version 1
+#def roll
+  #rand(1..6)
+#end
+
 def roll
-  # code goes here
+  a = [1, 2, 3, 4, 5, 6]
+  a.sample
 end
